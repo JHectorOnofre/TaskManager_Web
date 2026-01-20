da35d1105eace30693ec1a52e2b1976a7b56dfa6
caballerodavid2013170880-alt <caballero.david.2013170880@gmail.com>
2026-01-20 16:30:31 -0600

    Agregar archivos de proyecto.

---
+++ TaskManager.Web/Program.cs
@@ -0,0 +1,29 @@
+using TaskManager.Web.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddApiClients();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+//Esa línea define la ruta “clásica” (MVC) que ASP.NET Core usa para decidir qué controlador y qué acción ejecutar cuando llega una petición HTTP. Si llega una URL y no coincide con nada más específico,intenta interpretarla como:Controlador / Acción / Id opcional.
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Tasks}/{action=Index}/{id?}");
+
+app.Run();
---
+++ TaskManager.Web/Utilities/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TaskManager.Web.Services;
+
+namespace TaskManager.Web.Extensions
+{
+    public static class ServiceCollectionExtensions  //Inyeccion de dependecias sobre est metodo par ano tocar el Program.cs
+    {
+        public static IServiceCollection AddApiClients(this IServiceCollection services)
+        {
+            services.AddHttpClient<ITaskApiClient, TaskApiClient>();
+
+            return services;
+        }
+    }
+}
